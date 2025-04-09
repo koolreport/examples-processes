130869c4b57be27ba6a3373b9904e34251e596f1
dongnl <nguyenluongdong@gmail.com>
2025-04-09 15:19:58 +0700

    use getConfig() function in common instead of global variable

--- common.php
+++ common.php
@@ -10,37 +10,39 @@
 // require_once dirname(__FILE__) . "/vendor/autoload.php";
 require_once dirname(__FILE__)."/../koolreport/core/autoload.php";
 
-$config = array(
-    "automaker" => array(
-        "connectionString" => "mysql:host=sampledb.koolreport.com;dbname=automaker",
-        "username" => "expusr",
-        "password" => "koolreport sampledb",
-        "charset" => "utf8"
-    ),
-    "sakila" => array(
-        "connectionString" => "mysql:host=sampledb.koolreport.com;dbname=sakila",
-        "username" => "expusr",
-        "password" => "koolreport sampledb",
-        "charset" => "utf8"
-    ),
-    "world" => array(
-        "connectionString" => "mysql:host=sampledb.koolreport.com;dbname=world",
-        "username" => "expusr",
-        "password" => "koolreport sampledb",
-        "charset" => "utf8"
-    ),
-    "employees" => array(
-        "connectionString" => "mysql:host=sampledb.koolreport.com;dbname=employees",
-        "username" => "expusr",
-        "password" => "koolreport sampledb",
-        "charset" => "utf8"
-    ),
-    "salesCSV" => array(
-        'filePath' => '../../../databases/customer_product_dollarsales2.csv',
-        'class' => "\koolreport\datasources\CSVDataSource",
-        'fieldSeparator' => ';',
-    )
-);
+function getConfig() {
+    return array(
+        "automaker" => array(
+            "connectionString" => "mysql:host=sampledb.koolreport.com;dbname=automaker",
+            "username" => "expusr",
+            "password" => "koolreport sampledb",
+            "charset" => "utf8"
+        ),
+        "sakila" => array(
+            "connectionString" => "mysql:host=sampledb.koolreport.com;dbname=sakila",
+            "username" => "expusr",
+            "password" => "koolreport sampledb",
+            "charset" => "utf8"
+        ),
+        "world" => array(
+            "connectionString" => "mysql:host=sampledb.koolreport.com;dbname=world",
+            "username" => "expusr",
+            "password" => "koolreport sampledb",
+            "charset" => "utf8"
+        ),
+        "employees" => array(
+            "connectionString" => "mysql:host=sampledb.koolreport.com;dbname=employees",
+            "username" => "expusr",
+            "password" => "koolreport sampledb",
+            "charset" => "utf8"
+        ),
+        "salesCSV" => array(
+            'filePath' => '../../../databases/customer_product_dollarsales2.csv',
+            'class' => "\koolreport\datasources\CSVDataSource",
+            'fieldSeparator' => ';',
+        )
+    );
+}
 
 function getRootUrl()
 {
